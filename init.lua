20a9bb24e2b7ffa3cfc2d1f9f2cdbf4888ef8af7
DavidF <dfetzer@grayscale.io>
2024-05-29 11:18:52 -0400

    Transparency for windows terminal

--- init.lua
+++ init.lua
@@ -908,6 +908,45 @@ require('lazy').setup({
     end,
   },
 
+  -- Transparent
+  {
+    'xiyaowong/transparent.nvim',
+    config = function()
+      require('transparent').setup { -- Optional, you don't have to run setup.
+        groups = { -- table: default groups
+          'Normal',
+          'NormalNC',
+          'Comment',
+          'Constant',
+          'Special',
+          'Identifier',
+          'Statement',
+          'PreProc',
+          'Type',
+          'Underlined',
+          'Todo',
+          'String',
+          'Function',
+          'Conditional',
+          'Repeat',
+          'Operator',
+          'Structure',
+          'LineNr',
+          'NonText',
+          'SignColumn',
+          'CursorLine',
+          'CursorLineNr',
+          'StatusLine',
+          'StatusLineNC',
+          'EndOfBuffer',
+        },
+        extra_groups = {}, -- table: additional groups that should be cleared
+        exclude_groups = {}, -- table: groups you don't want to clear
+      }
+      require('transparent').clear_prefix 'NvimTree'
+    end,
+  },
+
   vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' }),
   vim.api.nvim_set_hl(0, 'TelescopeNormal', { bg = 'none' }),
   vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' }),
